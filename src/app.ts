16abe5c3c854f98de3fa35f2c0fdf1d5e7924388
noahlthorne <noahlthorne@gmail.com>
2021-05-31 11:16:39 +0900

    import routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,24 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application } from "express";
 require("dotenv").config();
 import config from "config";
 import log from "./logger";
 import connectDB from "./db/connect";
+import routes from "./routes";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
-const app: Application = express();
-
-// Connect to database
-connectDB();
+const app = express();
 
 // Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("hello");
-});
+app.listen(port, host, () => {
+    log.info(`Server listening at http://${host}:${port}`);
 
-app.listen(port, host, () =>
-    log.info(`Server listening at http://${host}:${port}`)
-);
+    // Connect to database
+    connectDB();
+
+    routes(app);
+});
