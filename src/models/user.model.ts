bef7233c7c3ae7d20d6a6061f3a4fe5b2fc3c1e5
noahlthorne <noahlthorne@gmail.com>
2021-05-31 14:01:09 +0900

    add user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,39 +1,68 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
 
-export interface IUser extends Document {
+export interface UserDocument extends Document {
     firstName: string;
     lastName: string;
     email: string;
     password: string;
     avatar: string;
-    date: Date;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
-const UserSchema: Schema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
+const UserSchema: Schema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+        },
     },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    avatar: {
-        type: String,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+    { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next: mongoose.HookNextFunction) {
+    let user = this as UserDocument;
+
+    //only hash the password if it has been modified or is new
+    if (!user.isModified("password")) return next();
+
+    // hash the password
+    const salt = await bcrypt.genSalt(config.get("saltWorkFactor"));
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    // Replace raw password with hashed password
+    user.password = hash;
+
+    return next();
 });
 
-export default mongoose.model<IUser>("User", UserSchema);
+// Compare password when logging in
+UserSchema.methods.comparePassword = async function (
+    candidatePassword: string
+) {
+    const user = this as UserDocument;
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+const User = mongoose.model<UserDocument>("User", UserSchema);
+
+export default User;
