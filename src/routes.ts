bd02dde2b6e2883acec2413eefa45d85c2f6217c
noahlthorne <noahlthorne@gmail.com>
2021-05-31 14:01:32 +0900

    add create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,27 @@
 import { Express, Request, Response, NextFunction } from "express";
+import { createUserHandler } from "./controllers/user.controller";
+import { createUserSchema } from "./schemas/user.schema";
+import validateRequest from "./middleware/validateRequest";
 
 export default function (app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) =>
         res.sendStatus(200)
     );
 
-    app.get("/", (req: Request, res: Response, next: NextFunction) => {
-        res.send("hello");
-    });
+    // Reister user
+    app.post(
+        "/api/users",
+        validateRequest(createUserSchema),
+        createUserHandler
+    );
+    // Post /api/user
+
+    // Login
+    // POST /api/sessions
+
+    // Get the user's session
+    // GET /api/sessions
+
+    // Logout
+    // DELETE /api/sessions
 }
