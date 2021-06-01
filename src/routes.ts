9f8f1cea83fba88f4e5a7c358605e259747a42d4
noahlthorne <noahlthorne@gmail.com>
2021-06-01 11:23:01 +0900

    add user and game routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,51 @@
 import { Express, Request, Response, NextFunction } from "express";
-import { createUserHandler } from "./controllers/user.controller";
-import { createUserSchema } from "./schemas/user.schema";
-import validateRequest from "./middleware/validateRequest";
+import { createUserHandler } from "./controllers/users.controller";
+import {
+    createUserSessionHandler,
+    invalidateUserSessionHandler,
+    getUserSessionsHandler,
+} from "./controllers/sessions.controller";
+import {
+    createGameHandler,
+    getGameHandler,
+} from "./controllers/games.controller";
+import {
+    createUserSchema,
+    createUserSessionSchema,
+} from "./schemas/user.schema";
+import { createGameSchema } from "./schemas/game.schema";
+import { validateRequest, requiresUser } from "./middleware";
 
 export default function (app: Express) {
-    app.get("/healthcheck", (req: Request, res: Response) =>
-        res.sendStatus(200)
-    );
-
+    // USERS
     // Reister user
     app.post(
         "/api/users",
         validateRequest(createUserSchema),
         createUserHandler
     );
-    // Post /api/user
 
     // Login
-    // POST /api/sessions
+    app.post(
+        "/api/sessions",
+        validateRequest(createUserSessionSchema),
+        createUserSessionHandler
+    );
 
     // Get the user's session
-    // GET /api/sessions
+    app.get("/api/sessions", requiresUser, getUserSessionsHandler);
 
     // Logout
-    // DELETE /api/sessions
+    app.delete("/api/sessions", requiresUser, invalidateUserSessionHandler);
+
+    // GAMES
+    // Create a game
+    app.post(
+        "/api/games",
+        [requiresUser, validateRequest(createGameSchema)],
+        createGameHandler
+    );
+
+    // Get a game
+    app.get("/api/games/:gameId", getGameHandler);
 }
