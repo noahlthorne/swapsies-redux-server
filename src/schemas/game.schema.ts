99c0eaf021c40acc5295064022d7d53fe15a5f07
noahlthorne <noahlthorne@gmail.com>
2021-06-01 11:49:19 +0900

    refactor create game to use payload

--- src/schemas/game.schema.ts
+++ src/schemas/game.schema.ts
@@ -1,6 +1,6 @@
 import { object, string, number, date } from "yup";
 
-export const createGameSchema = object({
+const payload = {
     body: object({
         title: string().required("Title is required"),
         gameConsole: string().required("console is required"),
@@ -10,4 +10,8 @@ export const createGameSchema = object({
         rating: number().required("rating is required").positive().integer(),
         releaseDate: string().required("release date is required"),
     }),
+};
+
+export const createGameSchema = object({
+    ...payload,
 });
