30e7a65a01373c963421cb5a6a4406a3bc8d2cc1
noahlthorne <noahlthorne@gmail.com>
2021-06-01 11:25:40 +0900

    add user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,5 @@
-import { DocumentDefinition } from "mongoose";
+import { omit } from "lodash";
+import { DocumentDefinition, FilterQuery } from "mongoose";
 import User, { UserDocument } from "../models/user.model";
 
 export const createUser = async (input: DocumentDefinition<UserDocument>) => {
@@ -9,4 +10,27 @@ export const createUser = async (input: DocumentDefinition<UserDocument>) => {
     }
 };
 
-export const findUser = async () => {};
+export const findUser = async (query: FilterQuery<UserDocument>) => {
+    return User.findOne(query).lean();
+};
+
+export const validatePassword = async ({
+    email,
+    password,
+}: {
+    email: UserDocument["email"];
+    password: string;
+}) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        return false;
+    }
+
+    const isValid = await user.comparePassword(password);
+
+    if (!isValid) {
+        return false;
+    }
+
+    return omit(user.toJSON(), "password");
+};
