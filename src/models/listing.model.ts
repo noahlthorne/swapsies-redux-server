fa5a6192a15a1a2194e39d1b785472e78d289e26
noahlthorne <noahlthorne@gmail.com>
2021-06-02 16:09:25 +0900

    add listing model

---
+++ src/models/listing.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { UserDocument } from "./user.model";
+import { GameDocument } from "./game.model";
+
+export interface ListingDocument extends Document {
+    user: UserDocument["_id"];
+    game: GameDocument["_id"];
+    condition: String;
+    status: String;
+}
+
+const ListingSchema: Schema = new Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        game: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
+        condition: { type: String },
+        status: { type: String, default: "available" },
+    },
+    { timestamps: true }
+);
+
+const Listing = mongoose.model<ListingDocument>("Listing", ListingSchema);
+
+export default Listing;
