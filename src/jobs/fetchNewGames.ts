dbddb7eb175e7d2d6f7db6b52e4673ff2833ca46
noahlthorne <noahlthorne@gmail.com>
2021-06-02 11:34:38 +0900

    add fetch new games job

---
+++ src/jobs/fetchNewGames.ts
@@ -0,0 +1,75 @@
+import axios from "axios";
+import config from "config";
+import log from "../logger";
+import { createGame } from "../services/game.service";
+
+interface gameData {
+    id: number;
+    aggregated_rating: number;
+    cover: {
+        id: number;
+        url: string;
+    };
+    summary: string;
+    first_release_date: number;
+    genres: [{ id: number; name: string }];
+    name: string;
+    platforms: [{ id: number; name: string }];
+}
+
+const fetchNewGames = async () => {
+    console.log("fetching game data...");
+    const headers = {
+        "Client-ID": config.get("clientId") as string,
+        // prettier-ignore
+        "Authorization": config.get("igdbAuth") as string,
+    };
+    const queryString =
+        "fields id,name,platforms.name,summary,first_release_date,genres.name,aggregated_rating,cover.url; limit 50; where aggregated_rating > 90 & platforms = (48,49,130,6); sort first_release_date desc;";
+
+    try {
+        const response = await axios.post(
+            "https://api.igdb.com/v4/games",
+            queryString,
+            { headers: headers }
+        );
+        response.data.forEach((game: gameData) => {
+            createGamesFromPlatforms(game);
+        });
+    } catch (error) {
+        log.error(error.message);
+    }
+};
+
+const createGamesFromPlatforms = (game: gameData) => {
+    game.platforms.forEach(async (platform) => {
+        const gameObj = buildGame(game, platform.name);
+        await createGame(gameObj);
+    });
+};
+
+const buildGame = (game: gameData, platform: string) => {
+    const { name, summary, cover, aggregated_rating, first_release_date } =
+        game;
+    const milliseconds = first_release_date * 1000;
+    const releaseDate = new Date(milliseconds);
+    let genres: any = game.genres;
+    if (genres) {
+        genres = genres.map((genre: any) => {
+            return genre.name;
+        });
+    }
+    const coverImage = `https:${cover.url}`.replace("t_thumb", "t_cover_big");
+    const gameObj = {
+        title: name,
+        gameConsole: platform,
+        genres: genres || [],
+        description: summary,
+        coverImage: coverImage,
+        rating: aggregated_rating,
+        releaseDate: releaseDate,
+    };
+    return gameObj;
+};
+
+export default fetchNewGames;
