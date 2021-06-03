7f6b92a7fb40e929ca8274f99f7c5540fee8bed2
noahlthorne <noahlthorne@gmail.com>
2021-06-03 17:22:28 +0900

    made status in schema optional

--- src/schemas/listing.schema.ts
+++ src/schemas/listing.schema.ts
@@ -3,7 +3,7 @@ import { object, string, number, array } from "yup";
 const payload = {
     body: object({
         condition: string().required("condition is required"),
-        status: string().required("status is required"),
+        status: string(),
     }),
 };
 
