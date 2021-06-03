0da7a8fd675b936e2cc51819efc3a1a260cee40b
noahlthorne <noahlthorne@gmail.com>
2021-06-03 17:21:37 +0900

    add listings controller

---
+++ src/controllers/listings.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { get } from "lodash";
+import { createListing, findGameListings } from "../services/listing.service";
+
+export const createListingHandler = async (req: Request, res: Response) => {
+    const userId = get(req, "user._id");
+    const gameId = get(req, "params.gameId");
+    const { condition } = req.body;
+    const listing = await createListing({
+        user: userId,
+        game: gameId,
+        condition,
+    });
+    res.send(listing);
+};
+
+export const getGameListingsHandler = async (req: Request, res: Response) => {
+    const gameId = get(req, "params.gameId");
+    const sessions = await findGameListings({ game: gameId });
+
+    return res.send(sessions);
+};
