5726615f786d8a8b296c25d986797fc449dfb0dd
noahlthorne <noahlthorne@gmail.com>
2021-06-14 11:59:38 +0900

    save image file using multer

--- src/routes/listings.routes.ts
+++ src/routes/listings.routes.ts
@@ -5,13 +5,41 @@ import {
 } from "../controllers/listings.controller";
 import { createListingSchema } from "../schemas/listing.schema";
 import { validateRequest, requiresUser } from "../middleware";
+import multer from "multer";
 
 const listingsRouter = Router();
+
+const MIME_TYPE_MAP = {
+    "image/png": "png",
+    "image/jpeg": "jpeg",
+    "image/jpg": "jpg",
+};
+
+const storage: any = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid =
+            MIME_TYPE_MAP[file.mimetype as keyof typeof MIME_TYPE_MAP];
+        let error: Error | null = new Error("Invalid mime type");
+        if (isValid) {
+            error = null;
+        }
+        cb(error, "src/images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname.toLowerCase().split(" ").join("-");
+        const ext = MIME_TYPE_MAP[file.mimetype as keyof typeof MIME_TYPE_MAP];
+        cb(null, `${name}-${Date.now()}.${ext}`);
+    },
+});
 
 // Create a listing
 listingsRouter.post(
     "/api/games/:gameId/listings",
-    [requiresUser, validateRequest(createListingSchema)],
+    [
+        requiresUser,
+        validateRequest(createListingSchema),
+        multer(storage).single("image"),
+    ],
     createListingHandler
 );
 
