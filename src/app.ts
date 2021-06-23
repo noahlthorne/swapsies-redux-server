98b54abdc5d05d35b6d3eb9ab76ee43f6180388d
noahlthorne <noahlthorne@gmail.com>
2021-06-23 12:41:23 +0900

    allow requests from front end app

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,25 @@ import "./jobs/cronJobs";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string | "0.0.0.0";
-
+const allowedOrigins = [
+    "http://localhost:4200",
+    "https://swapsies-3acb0.web.app",
+];
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:4200",
+        origin: function (origin, callback) {
+            // allow requests with no origin
+            // (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                let msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
     })
 );
 app.use(deserializeUser);
