b486fe9dd37adb8954e3e3724a96e6d153f73bee
noahlthorne <noahlthorne@gmail.com>
2021-06-23 12:02:53 +0900

    fixed build src

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,6 +1,6 @@
 import { Request } from "express";
-import { IUser } from "../models/user.model";
+import User from "../models/user.model";
 
 export interface IGetUserAuthInfoRequest extends Request {
-    user: IUser;
+    user: typeof User;
 }
