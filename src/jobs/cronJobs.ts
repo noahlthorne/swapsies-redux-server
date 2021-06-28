a64fc47b041d5a3eb06d29cb2955118e82f854b8
noahlthorne <noahlthorne@gmail.com>
2021-06-28 18:22:57 +0900

    removed console.log in cron job

--- src/jobs/cronJobs.ts
+++ src/jobs/cronJobs.ts
@@ -5,5 +5,4 @@ import log from "../logger";
 cron.schedule("0 0 0 * * *", async () => {
     log.info("Fetching new games from api...");
     await fetchNewGames();
-    log.info("Completed fetching new games");
 });
