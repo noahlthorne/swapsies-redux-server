c1b6f95041d1282764b61ec6e2da314397f1831a
noahlthorne <noahlthorne@gmail.com>
2021-06-30 11:27:00 +0900

    export swaps router from index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import usersRouter from "./users.routes";
 import gamesRouter from "./games.routes";
 import listingsRouter from "./listings.routes";
+import swapsRouter from "./swaps.routes";
 
 const routes = Router();
 
 routes.use(usersRouter);
-routes.use(listingsRouter);
 routes.use("/api/games", gamesRouter);
+routes.use(listingsRouter);
+routes.use(swapsRouter);
 
 export default routes;
