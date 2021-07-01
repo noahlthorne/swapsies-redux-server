c2a9fe84bbacbdc114a9d43d57c3b0635d257f4b
noahlthorne <noahlthorne@gmail.com>
2021-07-01 10:43:14 +0900

    add swaps controller

---
+++ src/controllers/swaps.controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { get } from "lodash";
+
+export const getUserSwapsHandler = async (req: Request, res: Response) => {
+    console.log("swaps controller");
+};
