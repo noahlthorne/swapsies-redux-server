d9d61f08bd93964897c36580b8f9d04085787672
noahlthorne <noahlthorne@gmail.com>
2021-07-01 10:52:29 +0900

    add get listing swaps handler

--- src/controllers/swaps.controller.ts
+++ src/controllers/swaps.controller.ts
@@ -4,3 +4,7 @@ import { get } from "lodash";
 export const getUserSwapsHandler = async (req: Request, res: Response) => {
     console.log("swaps controller");
 };
+
+export const getListingSwapsHandler = async (req: Request, res: Response) => {
+    console.log("get listing swaps");
+};
