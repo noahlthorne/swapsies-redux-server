101a2997a2bcceb1477b7db3fa5434b51a0f8eb5
noahlthorne <noahlthorne@gmail.com>
2021-07-01 11:02:24 +0900

    added create swap route

--- src/routes/swaps.routes.ts
+++ src/routes/swaps.routes.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import {
     getUserSwapsHandler,
     getListingSwapsHandler,
+    createSwapHandler,
 } from "../controllers/swaps.controller";
 import { validateRequest, requiresUser, extractFile } from "../middleware";
 
 const swapsRouter = Router();
 
 // Create a swap
-swapsRouter.post("/api/games/:gameId/swaps");
+swapsRouter.post("/api/listings/:listingId/swaps", createSwapHandler);
 
 // Get a listing's swaps
 swapsRouter.get("/api/listings/:listingId/swaps", getListingSwapsHandler);
