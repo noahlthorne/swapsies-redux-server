52e8252c980f163532b3d04fd355fee366428cf6
noahlthorne <noahlthorne@gmail.com>
2021-07-02 16:45:20 +0900

    add route handlers

--- src/controllers/swaps.controller.ts
+++ src/controllers/swaps.controller.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from "express";
+import { createSwap, findListingsSwaps } from "../services/swaps.service";
 import { get } from "lodash";
+import log from "../logger";
 
 export const getUserSwapsHandler = async (req: Request, res: Response) => {
     console.log("swaps controller");
 };
 
 export const getListingSwapsHandler = async (req: Request, res: Response) => {
-    console.log("get listing swaps");
+    const listingId = get(req, "params.listingId");
+    try {
+        const swaps = await findListingsSwaps({ listingRequested: listingId });
+        if (!swaps)
+            return res.status(404).send({ message: "Listing not found!" });
+        return res.send({ swaps: swaps });
+    } catch (error) {
+        return res.status(404).send({ message: "Error fetching swaps!" });
+    }
 };
 
 export const createSwapHandler = async (req: Request, res: Response) => {
-    console.log("create swap");
+    try {
+        const swap = await createSwap(req.body);
+        return res.status(201).send({ swap: swap });
+    } catch (error) {
+        log.info(error);
+        return res.status(404).send({ message: "Error saving swap" });
+    }
 };
