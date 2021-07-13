14f6523c5a10eb146995a35a9b4072be925a78b0
noahlthorne <noahlthorne@gmail.com>
2021-07-13 12:55:59 +0200

    add s3 file

---
+++ src/middleware/s3.ts
@@ -0,0 +1,36 @@
+import S3 from "aws-sdk/clients/s3";
+import config from "config";
+import fs from "fs";
+
+const AwsBucketName: string = config.get("AwsBucketName");
+const AwsBucketRegion: string = config.get("AwsBucketRegion");
+const AwsAccessKey: string = config.get("AwsAccessKey");
+const AwsSecretKey: string = config.get("AwsSecretKey");
+
+const s3 = new S3({
+    region: AwsBucketRegion,
+    accessKeyId: AwsAccessKey,
+    secretAccessKey: AwsSecretKey,
+});
+
+// Uploads a file to s3
+export const upload = (file: any) => {
+    const fileStream = fs.createReadStream(file.path);
+
+    const uploadParams = {
+        Bucket: AwsBucketName,
+        Key: file.filename,
+        Body: fileStream,
+    };
+
+    return s3.upload(uploadParams).promise();
+};
+
+// Downloads a file from s3
+export const download = (fileKey: string) => {
+    const downloadParams = {
+        Bucket: AwsBucketName,
+        Key: fileKey,
+    };
+    return s3.getObject(downloadParams).createReadStream();
+};
