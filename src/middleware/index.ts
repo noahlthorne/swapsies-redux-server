500bb1dd4e1c3bcf75f12eca0fb765527c66fd23
noahlthorne <noahlthorne@gmail.com>
2021-07-13 12:59:02 +0200

    export s3 functions from index file

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,3 +2,4 @@ export { default as deserializeUser } from "./deserializeUser";
 export { default as requiresUser } from "./requiresUser";
 export { default as validateRequest } from "./validateRequest";
 export { default as extractFile } from "./fileUpload";
+export { upload, download } from "./s3";
